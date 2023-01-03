8304ec91004bd13c55ad78a18ad2091f6617487c
Greg Molnar <molnar.gly@gmail.com>
2023-01-03 14:02:38 +0100

    Console log removed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import MainList, { adder, subtractor } from './pages/MainList'
+import MainList from './pages/MainList';
 
 const App = () => {
-  console.log(subtractor(3, 7))
   return (
     <>
       <div>Hello World</div>
       <MainList />
     </>
   );
-}
+};
 
 export default App;
